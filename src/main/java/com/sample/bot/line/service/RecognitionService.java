3db4f356ccc7fc4f24770f572ac3a2072dcfac61
e-craftsman <tamotsu_watanabe@e-craftsman.com>
2020-07-25 11:09:25 +0900

    First Commit

--- src/main/java/com/sample/bot/line/service/RecognitionService.java
+++ src/main/java/com/sample/bot/line/service/RecognitionService.java
@@ -6,22 +6,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.visual_recognition.v3.model.ClassResult;
+import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
+import com.ibm.watson.visual_recognition.v3.VisualRecognition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
-
-import com.ibm.watson.developer_cloud.service.security.IamOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassResult;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions.AcceptLanguage;
 
 @Service
 public class RecognitionService {
 
     @Value("${ibm.apiKey}")
     String apiKey;
+
+    @Value("${ibm.url}")
+    String url;
 
     public String detect(InputStream is) throws IOException {
         // 一時ファイル作成
@@ -32,22 +33,24 @@ public class RecognitionService {
         }
 
         // IBM API
-        VisualRecognition recognition = new VisualRecognition("2018-03-19", new IamOptions.Builder().apiKey(apiKey).build());
+        IamAuthenticator authenticator = new IamAuthenticator(apiKey);
+        VisualRecognition visualRecognition = new VisualRecognition("2018-03-19", authenticator);
+        visualRecognition.setServiceUrl(url);
 
         // Visual Recognition
         ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
                 .imagesFile(tempFile)
-                .acceptLanguage(AcceptLanguage.JA)
+                .acceptLanguage(ClassifyOptions.AcceptLanguage.JA)
                 .build();
 
         // 物体検出
-        ClassifiedImages result = recognition.classify(classifyOptions).execute();
+        ClassifiedImages result = visualRecognition.classify(classifyOptions).execute().getResult();
 
         List<ClassResult> classes = result.getImages().get(0).getClassifiers().get(0).getClasses();
 
         String res = "";
         for (ClassResult cls: classes) {
-            res += cls.getClassName() + ": " + Math.round(cls.getScore()) + "%" + System.getProperty("line.separator");
+            res += cls.getXClass() + ": " + Math.round(cls.getScore()) + "%" + System.getProperty("line.separator");
         }
         return res;
     }
